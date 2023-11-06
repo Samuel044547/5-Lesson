87f55dc90312982ff441b8c0989ff7f57f7a6ff1
Samuel044547 <148778928+Samuel044547@users.noreply.github.com>
2023-11-06 19:21:03 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,70 @@
+public class Main {
+    public static void main(String[] args) {
+//       1 esep
+//       double kabarga=5;
+//       double peremetr=kabarga*4;
+//       System.out.println("peremetr"+peremetr);
+//       2 esep
+//       double x=3;
+//       double y=4;
+//       double a=x+y/2;
+//       double b=Math.sqrt(x+y);
+//       System.out.println("a="+a );
+//       System.out.println("b="+b);
+//       3 esep
+//       double x=41;
+//       double y=19;
+//       double k=x+y;
+//       double a=x-y;
+//       double r=x*y;
+//       double l=x/y;
+//       System.out.println("a="+a );
+//       System.out.println("k="+k );
+//       System.out.println("r="+r );
+//       System.out.println("l="+l );
+//       4 esep
+//       int k=234;
+//       int g=k/7;
+//       System.out.println("g="+g);
+//       5 esep
+//       int x=23;
+//       int d=43;
+//       int o=x/10%10;
+//       int l=d/1%10;
+//       int f=o+l;
+//       int h=o*l;
+//       System.out.println("o="+o);
+//       System.out.println("k="+l);
+//       System.out.println("f="+f);
+//       System.out.println("h="+h);
+//       6 esep
+//       int x=237;
+//       int d=439;
+//       int o=x/100%10;
+//       int l=d/10%10;
+//       System.out.println("o="+o);
+//       System.out.println("k="+l);
+//       8 esep
+//       double x=356;
+//       double y=827;
+//       double m=x+y;
+//       double n=x*y;
+//       System.out.println("m="+m);
+//       System.out.println("n="+n);
+//       9 esep
+//       int c= 123;
+//       int can1 =c/1%10;
+//       int can2 =c/10%10;
+//       int can3 =c/100;
+//       System.out.println("can1="+can1+can2+can3);
+//       System.out.println("can2="+can2+can3+can1);
+//       System.out.println("can3="+can3+can2+can1);
+//       10 esep
+//       double r=8234;
+//       double j=9832;
+//       double e=r*j;
+//       double k=r+j;
+//       System.out.println("e="+e);
+//       System.out.println("k="+k);
+    }}
+
